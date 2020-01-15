babb0422b1579fe2bd98de17d043e0aa524e285d
Logan Waite <lwaite2010@gmail.com>
2020-01-15 09:45:41 -0600

    start routing

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,19 @@
 module Main exposing (..)
 
+-- import Element.Input as Input
+-- import Element.Lazy exposing (lazy)
+-- import Features.EditBrand
+-- import Features.ExportStyleGuide
+
 import Browser
-import Dashboard
-import Element exposing (Attribute, Element, alignLeft, alignRight, column, el, fill, height, padding, row, text, width)
+import Browser.Navigation as Nav
+import Element exposing (Attribute, Element, alignLeft, alignRight, column, el, fill, height, link, padding, row, text, width)
 import Element.Border as Border
 import Element.Font as Font
-import Element.Input as Input
 import Html exposing (Html)
 import UIHelpers exposing (borderWidth, textEl)
+import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser, s, string)
 
 
 
@@ -15,23 +21,35 @@ import UIHelpers exposing (borderWidth, textEl)
 
 
 type alias Model =
-    { feature : Maybe Feature
-    }
-
-
-initialModel =
-    { feature = Nothing
+    { feature : Feature
+    , key : Nav.Key
     }
 
 
 type Feature
-    = ExportStyleGuide
+    = Dashboard
+    | ExportStyleGuide
     | EditBrand
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, Cmd.none )
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( { feature = urlToFeature url, key = key }, Cmd.none )
+
+
+urlToFeature : Url -> Feature
+urlToFeature url =
+    Parser.parse parser url
+        |> Maybe.withDefault Dashboard
+
+
+parser : Parser (Feature -> a) a
+parser =
+    Parser.oneOf
+        [ Parser.map Dashboard Parser.top
+        , Parser.map EditBrand (s "edit")
+        , Parser.map ExportStyleGuide (s "export")
+        ]
 
 
 
@@ -39,65 +57,87 @@ init =
 
 
 type Msg
-    = ChangeFeature (Maybe Feature)
-    | GotDashboardMsg Dashboard.Msg
+    = ClickedLink Browser.UrlRequest
+    | ChangedUrl Url
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ChangeFeature feature ->
-            ( { model | feature = feature }, Cmd.none )
+        ClickedLink urlRequest ->
+            case urlRequest of
+                Browser.External href ->
+                    ( model, Nav.load href )
 
-        GotDashboardMsg dashboardMsg ->
-            ( model, Cmd.none )
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+        ChangedUrl url ->
+            ( { model | feature = urlToFeature url }, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
+    { title = "Brandtrics"
+    , body =
+        [ layout model
+        ]
+    }
+
+
+layout : Model -> Html Msg
+layout model =
     Element.layout
         [ height fill, width fill ]
     <|
         column
             [ width fill ]
-            [ row
-                [ width fill
-                , padding 15
-                , Border.widthEach { borderWidth | bottom = 2 }
-                ]
-                [ textEl [ alignLeft, Font.bold ] "Brandtrics"
-                , textEl [ alignRight ] "menu"
-                ]
-            , row
-                []
-                [ featureButton "Export Style Guide" ExportStyleGuide
-                , featureButton "Edit Brand" EditBrand
-                ]
+            [ header
             , featureScreen model.feature
             ]
 
 
-featureScreen : Maybe Feature -> Element Msg
+header : Element Msg
+header =
+    row
+        [ width fill
+        , padding 15
+        , Border.widthEach { borderWidth | bottom = 2 }
+        ]
+        [ textEl [ alignLeft, Font.bold ] "Brandtrics"
+        , textEl [ alignRight ] "menu"
+        ]
+
+
+featureScreen : Feature -> Element Msg
 featureScreen feature =
     case feature of
-        Just ExportStyleGuide ->
+        ExportStyleGuide ->
             textEl [] "Export Style Guide"
 
-        Just EditBrand ->
+        EditBrand ->
             textEl [] "Edit Brand"
 
-        Nothing ->
-            Dashboard.view Dashboard.Model
-                |> Element.map GotDashboardMsg
+        Dashboard ->
+            dashboard
 
 
-featureButton : String -> Feature -> Element Msg
-featureButton label feature =
-    Input.button [ padding 50 ] { onPress = Just (ChangeFeature (Just feature)), label = textEl [] label }
+featureButton : String -> String -> Element Msg
+featureButton label url =
+    link [ padding 50 ] { url = url, label = textEl [] label }
+
+
+dashboard : Element Msg
+dashboard =
+    Element.wrappedRow
+        []
+        [ featureButton "Export Style Guide" "/export"
+        , featureButton "Edit Brand" "/edit"
+        ]
 
 
 
@@ -106,9 +146,11 @@ featureButton label feature =
 
 main : Program () Model Msg
 main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
+    Browser.application
+        { init = init
+        , onUrlRequest = ClickedLink
+        , onUrlChange = ChangedUrl
+        , subscriptions = \_ -> Sub.none
         , update = update
-        , subscriptions = always Sub.none
+        , view = view
         }
